10ab5ec7f637e8dc732779b388b9c7e80d6402ee
Aon Muhammad <145316686+AonAideveloper@users.noreply.github.com>
2023-12-04 12:09:25 +0500

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 10:07:29 2023
+
+@author: web 96
+"""
+
+#print(7*8)
+
+##phrase_1 =  'The cat sat on the mat !'
+##phrase_2 = 'And so did the dog !'
+##phrase_3 = phrase_1 + phrase_2 
+
+##radius = float(input('please enter the circle radius :'))
+#PI = 3.14159
+#area = PI*radius**2
+#print('Your entered', radius , 'the area of a circule is' , area )
+
+##far = float(input('Please Enter the temperature in fahrenheit:'))
+##cel = (far-32)*5/9
+##print(far, 'fahrenheit in celcius', cel)
+##num_1 = int(input('please enter the first number :'))
+##num_2 = int(input('please enter the second number :'))
+##product = num_1 * num_2
+##print('the product of' , num_1 , 'and', num_2, 'is', product)
+
+
+##total_slices = 4*4
+##number_of_pizzas = (total_slices + 5)//6
+##slices_left = number_of_pizzas*6 - total_slices
+##print('Number of pizzas required is' , number_of_pizzas, 'there will be', slices_left, 'remaing slices.')
+
+# total_money = 560 * 560
+# total_person =  6
+# number_of_person = (total_person + 2) // 4
+# money_left = total_person * 4 - total_money
+# print('Number of person required is', number_of_person, 'there will be', money_left, 'remaining money')
+
+# cel = float(input('enter the temperature in celcius :'))
+# far = (cel*9/5)+32
+# print(cel,'Celcius in farenheit', far)
+
+# length = int(input('Please Enter the length :'))
+# width = int(input('please enter the width :'))
+# product = length * width
+
+# print('The product of' , length , 'and', width, 'is', product)
+
+# rad = float(input('Enter the radius:'))
+# PI = 3.14159
+# circumference  = PI*rad**2
+# print('your entered', rad, 'the circumference of area is', circumference)
+# rad = float(input('Enter the radius: '))
+# PI = 3.14159
+# circumference = 2 * PI * rad
+# print('You entered', rad, 'and the circumference is', circumference)
+
+# number_1 = int(input('Enter the number :'))
+# sum_of_digits = 0
+# while number_1>0:
+#     digit = number_1 / 10
+#     sum_of_digits += digit
+# number_1 //=10
+# print('you entered number', number_1, 'and the sum of its digits is', sum_of_digits)
+
